b0c9be815b9f7c3866aefb4a2ce2c5e1d12e575b
znha <zihna-19@hotmail.com>
2020-09-13 16:41:56 +0300

    setting an example with muidatatables

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import MUIDataTable from "mui-datatables";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Client = (props) => (
+  <>
+    <a href="https://github.com/" className="btn btn-primary">
+      Edit
+      </a>
+      &nbsp;
+    <a
+      href="https://github.com/"
+      onClick={() => {
+        props.deleteClient(props.client._id);
+      }}
+      className="btn btn-danger"
+    >
+      Delete
+      </a>
+
+  </>
+);
+
+export default class ClientsList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteClient = this.deleteClient.bind(this);
+
+    this.state = {
+      clients: [{
+
+      }]
+    };
+  }
+
+  componentDidMount() {
+
+    this.setState({
+      clients: [{
+        _id: 1,
+        name: 'zinah',
+
+      },
+      {
+        _id: 2,
+        name: 'mona',
+
+      }]
+    });
+
+  }
+
+  deleteClient(id) {
+
+  }
+
+  // This is the map I was talking about:
+
+
+  clientList(currentclient) {
+    console.log('heloooooooooooooooooooooooooooo',currentclient);
+      return (
+        <Client
+          client={currentclient}
+          deleteClient={this.deleteClient}
+          key={1}
+        />
+      );
+  
+  }
+
+  render() {
+    const columns = [
+      {
+        name: "name",
+        label: "Name",
+        options: {
+          filter: true,
+          sort: true,
+        },
+      },
+      {
+        name: "_id",
+        options: {
+          display: false,
+        }
+      }, 
+      {
+        name: "Action",
+        options: {
+          customBodyRender: (value, tableMeta, updateValue) => {
+            return <>{this.clientList(tableMeta.rowData)}</>;
+          },
+        },
+      },
+    ];
+    const { clients } = this.state;
+
+    return (
+      <>
+        
+        <div style={{ margin: "10px 15px", overflowX: "auto" }}>
+          
+          <MUIDataTable data={clients} columns={columns} />
+        </div>
+      </>
+    );
+  }
 }
 
-export default App;
